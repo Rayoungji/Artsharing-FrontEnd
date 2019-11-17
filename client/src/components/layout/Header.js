ff7e5b0cc191d11ecfcbcd1d0995ebb0d10bbb89
Rayoungji <dudwl7676@naver.com>
2019-11-17 22:53:56 +0900

    회원가입 수정

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -59,8 +59,8 @@ class Blog extends React.Component {
                        <Grid container sm="4" />
                        <Grid container sm="2">
                            <Button color="inherit" href="/SignUp">회원가입</Button>
-                           {isLogin == false && <Login handleLogin={this.handleLogin} />}
-                           {isLogin == true && <Button onClick={this.handleLogout}>Logout</Button>}
+                           {isLogin === false && <Login handleLogin={this.handleLogin} />}
+                           {isLogin === true && <Button onClick={this.handleLogout}>Logout</Button>}
                        </Grid>
                    </Toolbar>
                    <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
@@ -68,7 +68,7 @@ class Blog extends React.Component {
                        <Link to="/artistList"> <Button color="inherit">작가보기</Button></Link>
                        <Button color="inherit" href="/artistOnly">작품관리</Button>
                        <Button color="inherit" href="/rentList">대여관리</Button>
-                       {isLogin == true && <Link to="/profile"><Button color="inherit">회원정보</Button></Link>}
+                       {isLogin === true && <Link to="/profile"><Button color="inherit">회원정보</Button></Link>}
 
 
                    </Toolbar>
--- client/src/containers/SignUp.js
+++ client/src/containers/SignUp.js
@@ -1,180 +1,274 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Checkbox from '@material-ui/core/Checkbox';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import PropTypes from 'prop-types';
-import {  useTheme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import PropTypes from "prop-types";
+import { useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
+    "@global": {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
     },
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const {
+        children, value, index, ...other
+    } = props;
 
-  return (
-    <Typography
-      align="center"
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
+    return (
+        <Typography
+            align="center"
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            {...other}
+        >
+            <Box p={3}>{children}</Box>
+        </Typography>
+    );
 }
 
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
 };
 
 
 export default function SignUp() {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
           Sign up
-        </Typography>
-        <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="action tabs example"
-        >
-          <Tab label="작가/개인" />
-          <Tab label="기업" />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0} dir={theme.direction}>
-      <form className={classes.form} noValidate>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-            작가<Checkbox/> 개인<Checkbox/>
-          </Grid>
-      <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="Name"
-                label="User Name"
-                name="Name"
-                autoComplete="Name"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="sex"
-                name="sex"
-                variant="outlined"
-                required
-                fullWidth
-                id="sex"
-                label="sex"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="age"
-                name="age"
-                variant="outlined"
-                required
-                fullWidth
-                id="age"
-                label="age"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="id"
-                label="id"
-                name="id"
-                autoComplete="id"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
+                </Typography>
+                <AppBar position="static" color="default">
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="fullWidth"
+                        aria-label="action tabs example"
+                    >
+                        <Tab label="작가/개인" />
+                        <Tab label="기업" />
+                    </Tabs>
+                </AppBar>
+                <TabPanel value={value} index={0} dir={theme.direction}>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+            작가<Checkbox /> 개인<Checkbox />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="Name"
+                                    label="User Name"
+                                    name="Name"
+                                    autoComplete="Name"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="sex"
+                                    name="sex"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="sex"
+                                    label="sex"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="age"
+                                    name="age"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="age"
+                                    label="age"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="phone"
+                                    label="phone"
+                                    name="phone"
+                                    autoComplete="phone"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="id"
+                                    label="id"
+                                    name="id"
+                                    autoComplete="id"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
             Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-          </Grid>
-          </Grid>
-        </form>
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          Item Two
-        </TabPanel>      
-      </div>
-    </Container>
-  );
+                            </Button>
+                            <Grid container justify="flex-end" />
+                        </Grid>
+                    </form>
+                </TabPanel>
+                <TabPanel value={value} index={1} dir={theme.direction}>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="Name"
+                                    label="User Name"
+                                    name="Name"
+                                    autoComplete="Name"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="sex"
+                                    name="sex"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="sex"
+                                    label="sex"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="age"
+                                    name="age"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="age"
+                                    label="age"
+                                    autoFocus
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="phone"
+                                    label="phone"
+                                    name="phone"
+                                    autoComplete="phone"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="id"
+                                    label="id"
+                                    name="id"
+                                    autoComplete="id"
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                />
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+            Sign Up
+                            </Button>
+                            <Grid container justify="flex-end" />
+                        </Grid>
+                    </form>
+                </TabPanel>
+            </div>
+        </Container>
+    );
 }
